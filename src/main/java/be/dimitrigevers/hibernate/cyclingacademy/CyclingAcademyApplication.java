650ab1f1d7d5743e4df6ce1933a827716d0ea8c2
dmtwood <dimitri.gevers@gmail.com>
2021-09-23 15:16:45 +0200

    Initial commit

---
+++ src/main/java/be/dimitrigevers/hibernate/cyclingacademy/CyclingAcademyApplication.java
@@ -0,0 +1,13 @@
+package be.dimitrigevers.hibernate.cyclingacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyclingAcademyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyclingAcademyApplication.class, args);
+    }
+
+}
